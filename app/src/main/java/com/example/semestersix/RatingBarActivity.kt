2457a5784735c84457cbce62a794979f4d21abc3
Mebanseibor Lyngdoh <mebanseiborlm@gmail.com>
2025-02-28 02:10:51 +0530

    Added an interactive submit button for the rating bar - Added a button to mimic the submission of the rating - Added a toast message to display the rating inputted by the user

--- app/src/main/java/com/example/semestersix/RatingBarActivity.kt
+++ app/src/main/java/com/example/semestersix/RatingBarActivity.kt
@@ -1,6 +1,11 @@
 package com.example.semestersix
 
 import android.os.Bundle
+import android.util.Half.toFloat
+import android.util.Log
+import android.widget.Button
+import android.widget.RatingBar
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,5 +14,19 @@ class RatingBarActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_ratingbar)
+
+        initRatingStar()
+    }
+
+    private fun initRatingStar(){
+        val buttonSubmit = findViewById<Button>(R.id.submitButton)
+        val ratingBarMain = findViewById<RatingBar>(R.id.ratingBar)
+
+        buttonSubmit.setOnClickListener {
+            Log.d("buttonSubmit", "Submit Button was clicked")
+            val totalStars = ratingBarMain.numStars
+            val userRating = ratingBarMain.rating
+            Toast.makeText(this, """Rated $userRating/${totalStars.toFloat()} Stars""", Toast.LENGTH_SHORT).show()
+        }
     }
 }
